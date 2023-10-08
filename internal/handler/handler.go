c808393cbee147e54d00dcb7c93b1f367eff7af3
Pavel Chen <pchen@ozon.ru>
2023-10-08 18:38:38 +0300

    fix authorization

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -37,7 +37,7 @@ func InitRoutes(student StudentHandler, supervisor SupervisorHandler) *gin.Engin
 	router.GET("/students/science/:id", student.GetScientificWork)
 	router.GET("/supervisor/info/:id", supervisor.GetInfo)
 	router.GET("/supervisor/students/:id", supervisor.GetStudents)
-	router.GET("/authorize", student.Authorize)
+	router.POST("/authorize", student.Authorize)
 
 	return router
 }
